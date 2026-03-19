6e5f559d9891e1f5180ab3832174adefe26bdff4
Abel Scheidt <abel.scheidt@gmail.com>
2026-03-18 21:18:28 -0300

    criado classe Viewport, importado arquivos imgui, e criado dir program com a main

---
+++ src/gui/headers/viewport.h
@@ -0,0 +1,17 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+#include <math.h> // fmodf
+
+#include "log_app.h"
+
+class Viewport {
+public:
+    void run();
+};
